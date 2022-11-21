1b2c034e69eb0c72891266fb5416716c5caec985
M. Waqas Riaz <waqas.riaz@pucit.edu.pk>
2022-11-22 03:12:25 +0500

    test taking

--- skill_test/views.py
+++ skill_test/views.py
@@ -222,14 +222,18 @@ def tests(request):
         return redirect(index)
     if not request.user.is_authenticated:
         return redirect(index)
-    username = request.user.username
-    username = "wickybestge883"
-    u = Users.objects.filter(username=username)[0]
-    allowed_test = json.loads(u.allowed_test)
     tests = Tests.objects.all()
-    print(allowed_test)
-    t_ids = [t.t_id for t in tests if t.is_open and t.t_id in allowed_test]
-    email = u.email
+    t_ids = [t.t_id for t in tests if t.is_open]
+    if request.user.is_superuser:
+        allowed_test = t_ids
+        email = request.user.username+"@super.user"
+    else:
+        username = request.user.username
+        u = Users.objects.filter(username=username)[0]
+        email = u.email
+        allowed_test = json.loads(u.allowed_test)
+    t_ids = [t.t_id for t in tests if t.t_id in allowed_test]
+    t_ids = [t_id for t_id in t_ids if Questions.objects.filter(t_id=t_id)]
     if request.method == "POST":
         agree = bool(request.POST.get("agree"))
         t_id = request.POST.get("t_id")
@@ -248,13 +252,129 @@ def tests(request):
         }
     )
 
-def take_test(request, t_id):
+def take_test(request, t_id="", q_id=1):
     if not request.META.get('HTTP_REFERER'):
         return redirect(index)
     if not request.user.is_authenticated:
         return redirect(index)
-    print("haha",t_id)
-    return render(request, 'take_test.html', {"t_id":t_id})
+    if request.method == "POST":
+        if "previous" in request.POST:
+            t_id, q_id = request.POST.get("previous").split(",")
+        if "next" in request.POST:
+            t_id, q_id = request.POST.get("next").split(",")
+        if "verify" in request.POST:
+            t_id, q_id = request.POST.get("verify").split(",")
+        if "submit" in request.POST:
+            t_id, q_id = request.POST.get("submit").split(",")
+        q_id = int(q_id)
+    status = "Un Attempted"
+    testcases_flag = False
+    testcases = ""
+    prefilled_solution = "def solution(x):\n    # return your code"
+    questions = Questions.objects.filter(t_id=t_id)
+    total_questions = len(questions)
+    if q_id>total_questions:
+        messages.error(request, "Technical Error!")
+        return redirect(index)
+    username = request.user.username
+    if request.user.is_superuser:
+        email = username+"@super.user"
+    else:
+        u = Users.objects.filter(username=username)[0]
+        email = u.email
+        allowed_test = json.loads(u.allowed_test)
+        if t_id not in allowed_test:
+            messages.error(request, "You are not allowed to attempt this test!")
+            return redirect(index)
+    if request.method == "POST":
+        if "previous" in request.POST:
+            q_id -= 1
+            if q_id<1:
+                q_id = 1
+            print(q_id,t_id,"previous")
+        if "next" in request.POST:
+            q_id += 1
+            if q_id>=total_questions:
+                q_id = total_questions
+            print(q_id,t_id,"next")
+        if "verify" in request.POST or "submit" in request.POST:
+            try:
+                r_flag = False
+                results = Results.objects.filter(t_id=t_id, email=email, q_id=questions[q_id-1].q_id)
+                if results:
+                    r_flag = results[0]
+                    if r_flag.t_cases>0:
+                        status = "Submitted"
+                    else:
+                        status = "Not Submitted"
+            except:
+                pass
+
+            if status=="Submitted":
+                messages.error(request, "Already submitted!\nYou cant submit a solution twice.")
+            else:
+                solution = request.POST.get("solution")
+                groundtruths = json.loads(questions[q_id-1].groundtruths)
+                score = int(questions[q_id-1].score)
+                solution_flag, testcases, obt_marks = validate(solution, groundtruths, score)
+                testcases = testcases.replace('<table border="1"','<table style="border-style:dashed;border-color:#fffffF;border-width:thin;height:100px;width:100%;"')
+                testcases = testcases.replace('<th>','<th align="center">')
+                testcases_flag = True
+                if solution_flag:
+                    t_cases = len(groundtruths)
+                    p_cases = testcases.count("Passed")
+                    if "submit" in request.POST:
+                        if r_flag:
+                            r = r_flag
+                            r.obtained_marks = obt_marks
+                            r.soultion = solution
+                            r.t_cases = t_cases
+                            r.p_cases = p_cases
+                        else:
+                            r = Results(t_id=t_id, q_id=q_id, obtained_marks=obt_marks, total_marks=score, email=email, solution=solution, t_cases=t_cases, p_cases=p_cases)
+                        r.save()
+                        messages.success(request, 'Your solution has been submitted!')
+                    else:
+                        if r_flag:
+                            r = r_flag
+                            r.soultion = solution
+                            r.p_cases = p_cases
+                        else:
+                            r = Results(t_id=t_id, q_id=q_id, obtained_marks=0, total_marks=score, email=email, solution=solution, t_cases=0, p_cases=0)
+                        r.save()
+                        status = "Submitted"
+                        messages.success(request, 'Your solution has been Verified!\nPress submit button to submit it.')
+                else:
+                    status = "Not Submitted"
+                    messages.error(request, 'Unsuccessful!\nRemove errors from your code!')
+    try:
+        results = Results.objects.filter(t_id=t_id, email=email, q_id=questions[q_id-1].q_id)
+        if results:
+            results = results[0]
+            prefilled_solution = results.solution
+            if results.t_cases>0:
+                status = "Submitted"
+            else:
+                status = "Not Submitted"
+    except:
+        pass
+    
+    return render(
+        request,
+        'take_test.html',
+        {
+            "t_id": t_id,
+            "q_id": q_id,
+            "t_questions": total_questions,
+            "u_email": email,
+            "status": status,
+            "statement": questions[q_id-1].statement,
+            "score": int(questions[q_id-1].score),
+            "testcases_flag": testcases_flag,
+            "prefilled_solution": prefilled_solution,
+            "testcases": testcases
+        }
+    )
 
 def users_m(request, op=1, batches=[], all_t_ids=[]):
     try:
@@ -299,7 +419,13 @@ def users_m(request, op=1, batches=[], all_t_ids=[]):
                     messages.error(request, "Please fill all required fields i.e., name, email and password")
                 else:
                     if email in all_existing_emails:
-                        messages.error(request, "Email Already Exists. Please a different email.")
+                        u = Users.objects.filter(email=email)
+                        if u:
+                            messages.error(request, "Email Already Exists. Please a different email.")
+                        else:
+                            usr = Users(name=name, email=email, phone=phone, degree=degree, year=year, dob=dob, allowed_test=json.dumps([]), username="", password="", is_active=True)
+                            usr.save()
+                            messages.error(request, "Email Already Exists. Please a different email.")
                     else:
                         username = email.split("@")[0]
                         while username in all_existing_usernames:
@@ -378,7 +504,6 @@ def users_m(request, op=1, batches=[], all_t_ids=[]):
                             error_e += [email]
                     messages.success(request, "done for"+str(done_e)+(" failed for"+str(error_e) if error_e else ""))
                 except:
-                    print(traceback.print_exc())
                     messages.error(request, "Unable to Assign!")
             if "create_batch" in request.POST:
                 try:
@@ -481,13 +606,60 @@ def users_m(request, op=1, batches=[], all_t_ids=[]):
         }
     )
 
-def results(request):
+def results(request, op=2):
     if not request.META.get('HTTP_REFERER'):
         return redirect(index)
     if not request.user.is_authenticated:
         return redirect(index)
-    return render(request, 'results.html', {})
-
+    try:
+        tests = Tests.objects.all()
+        t_ids = [t.t_id for t in tests if t.is_open]
+        all_batches = Batches.objects.all()
+        b_ids = [b.b_id for b in all_batches if b.is_open]
+        superuserflag = False
+        search = ""
+        username = request.user.username
+        if request.user.is_superuser:
+            email = username+"@super.user"
+            superuserflag = True
+        else:
+            u = Users.objects.filter(username=username)[0]
+            email = u.email
+        if request.method == "POST":
+            if "by_t_link" in request.POST:
+                op = 1
+            if "by_e_link" in request.POST:
+                op = 2
+            if "search_email" in request.POST or "download_email" in request.POST:
+                if "search_email" in request.POST:
+                    op = int(request.POST.get("search_email"))
+                if "download_email" in request.POST:
+                    op = int(request.POST.get("download_email"))
+                email = request.POST.get("email")
+                print(email)
+            if "search_test" in request.POST or "download_test" in request.POST:
+                if "search_test" in request.POST:
+                    op = int(request.POST.get("search_test"))
+                if "download_test" in request.POST:
+                    op = int(request.POST.get("download_test"))
+                t_id = request.POST.get("t_id")
+                b_id = request.POST.get("b_id")
+                print(t_id,b_id)
+    except:
+        print(traceback.print_exc())
+    return render(
+        request,
+        'results.html',
+        {
+            "op": op,
+            "email": email,
+            "superuserflag": superuserflag,
+            "t_ids": t_ids,
+            "b_ids": b_ids,
+            "search": search
+            
+        }
+    )
 
 
 
